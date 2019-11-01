a4ffc7a9a6dd7fcc808b597390c14a2c585373ac
yutveg <kodi.0n31l@gmail.com>
2019-11-01 08:18:52 -0700

    Header Added

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,30 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+const entry = document.querySelector('.header-container');
+entry.appendChild(Header());
+
+function Header() {
+    //Creating elements and vars
+    const header = document.createElement('div');
+    const date = document.createElement('span');
+    const title = document.createElement('h1');
+    const temp = document.createElement('span');
+
+    //Appendages
+    header.appendChild(date);
+    header.appendChild(title);
+    header.appendChild(temp);
+    
+    //adding classes
+    header.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+    //adding content 
+    date.textContent="MARCH 28, 2019";
+    title.textContent="Lambda Times";
+    temp.textContent="98°";
+    
+    return header;
+}
